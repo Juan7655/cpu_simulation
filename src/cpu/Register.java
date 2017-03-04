d3263d29cf40718229dbf04fc166c5c7f6c6dcee
Juan David <juan.7655@hotmail.com>
2017-03-04 16:37:10 -0500

    Edited register Object

--- src/cpu/Register.java
+++ src/cpu/Register.java
@@ -13,15 +13,27 @@ import java.util.Stack;
  */
 public class Register {
     private final Stack<Integer> registries = new Stack<>();
+    private int currentValue = -1;
 
     public Register() {
     }
     
     public void putRegistry(int value){
         this.registries.add(value);
+        this.currentValue = value;
     }
     
     public int getLastRegistry(){
-        return this.registries.peek();
+        int temp = this.currentValue;
+        if(temp != -1)this.currentValue = -1;
+        
+        return temp;
+    }
+    
+    public Integer[] getAllItems(){
+        Integer[] array = new Integer[this.registries.size()];
+        this.registries.toArray(array);
+        
+        return array;
     }
 }
